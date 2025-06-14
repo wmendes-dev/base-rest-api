823aee3a0fc5579bb779a93e1b4a79df45f374c9
wmendes-dev <whdesenvolvedor@gmail.com>
2025-06-13 23:40:07 -0400

    Autenticação de usuários

---
+++ src/main/java/br/com/base_rest_api/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package br.com.base_rest_api.repository;
+
+import br.com.base_rest_api.domain.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByCpf(String cpf);
+
+}
